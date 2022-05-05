2dc699c132fde4b9bbedd5ad88f436517b6e35e2
prasanth.muthunadan <prasanth.muthunadan@DT150107.QICHOAPPL.com>
2022-05-05 13:08:49 +0530

    sfsd

--- jack/src/main/java/jack/Man.java
+++ jack/src/main/java/jack/Man.java
@@ -1,20 +1,11 @@
 package jack;
 
-import java.io.File;
-import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
 public class Man {
-	static Logger logger=Logger.getLogger(Man.class);
-	
+
 	public static void main(String[] args) {
-		String str="welcome to sonar";
-		
-		String log4jConfigFile = System.getProperty("user.dir") + File.separator + "log4j.properties";
-		PropertyConfigurator.configure(log4jConfigFile);
-		logger.info("this is a dmessage....:"+str);
-		logger.warn("warning message...:"+str);
-		logger.error("error rrtmessggage...:"+str);
-	
+		System.out.println("dfs");
+		// TODO Auto-generated method stub
+
 	}
 
 }
