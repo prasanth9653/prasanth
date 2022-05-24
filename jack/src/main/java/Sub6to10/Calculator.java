7f337be33db2e1d2460e429942318db1ea30c147
prasanth.muthunadan <prasanth.muthunadan@DT150107.QICHOAPPL.com>
2022-05-24 11:01:37 +0530

    class

---
+++ jack/src/main/java/Sub6to10/Calculator.java
@@ -0,0 +1,40 @@
+package Sub6to10;
+
+
+	public class Calculator {
+		void add1(int i,int j) {
+			int sum=i+j;
+			System.out.println("the sum of "+i+"and "+j+"is..:"+sum);
+		}
+		
+		int add2(int i,int j) {
+			return i+j;
+		}
+		
+		void add3(int i,int j) {
+			display(i+j);
+		}
+		
+		void display(int s) {
+			System.out.println("The result is..:"+s);
+		}
+		
+		void add4(int ...i) {
+			int result=0;
+			for(int s:i) {
+				result=result+s;
+			}
+			System.out.printf("The result is ...:%d", result);
+		}
+		
+		void add5(int i[]) {
+			int result=0;
+			for(int s:i) {
+				result=result+s;
+			}
+			System.out.printf("The result is ...:%d", result);
+			
+		}
+		
+	}
+
