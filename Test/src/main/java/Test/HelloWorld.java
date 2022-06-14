e43aac2a4ec94213d4f7b3fdd6bdd3bc1ba6d34a
Hp <Hp@LAPTOP-DT3SN1R0>
2022-06-14 16:00:25 +0530

    hi

--- Test/src/main/java/Test/HelloWorld.java
+++ Test/src/main/java/Test/HelloWorld.java
@@ -10,7 +10,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		String log4jConfigFile=System.getProperty("user.dir")+File.separator+"log4j.Properties";
 		PropertyConfigurator.configure(log4jConfigFile);
-		System.out.println("HelloWorld...s..........");
+		System.out.println("HelloWorld.............");
 		logger.info("Hello");
 	}
 
