3cdf7611827972252cae31a8573e1b436fa4b80b
Hp <Hp@LAPTOP-DT3SN1R0>
2022-06-14 11:47:51 +0530

    hi

---
+++ git/Sub6to10/ThisDemo2.java
@@ -0,0 +1,29 @@
+package Sub6to10;
+
+
+	public class ThisDemo2 {
+		String s;
+		public ThisDemo2(String s) {
+			this.s=s;
+		}
+		public static void main(String[] args) {
+			//this keyword cannot be used inside a static method
+			ThisDemo2 obj=new ThisDemo2("some value...");
+			obj.printS("aaaaaaaaaa");
+		}
+		
+		public void printS(String s) {
+			
+			System.out.println(this.s);
+		}
+	}
+	
+
+
+
+
+
+
+
+
+
