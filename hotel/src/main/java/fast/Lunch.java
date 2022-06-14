5fdb9eed12456aec013a4627a022bdd2e95fa1f6
Hp <Hp@LAPTOP-DT3SN1R0>
2022-06-14 15:57:13 +0530

    new

--- hotel/src/main/java/fast/Lunch.java
+++ hotel/src/main/java/fast/Lunch.java
@@ -3,7 +3,7 @@ package fast;
 public class Lunch {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello  ");
 		// TODO Auto-generated method stub
 
 	}
