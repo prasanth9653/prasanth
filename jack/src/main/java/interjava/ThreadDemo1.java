8dad8c660bac0d95dc8adf9b4781ce08a7090d67
prasanth.muthunadan <prasanth.muthunadan@DT150107.QICHOAPPL.com>
2022-06-14 11:50:02 +0530

    hi

---
+++ jack/src/main/java/interjava/ThreadDemo1.java
@@ -0,0 +1,17 @@
+package interjava;
+
+public class ThreadDemo1 {
+
+	public static void main(String[] args) {
+		System.out.println("line 1.....");
+		met();
+		System.out.println("Line 3...");
+
+	}
+
+
+public static void met()
+{
+	
+}
+}
