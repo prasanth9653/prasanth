8e83131c40a1198efaa511ad2fe8efd871485406
prasanth.muthunadan <prasanth.muthunadan@DT150107.QICHOAPPL.com>
2022-07-26 09:57:21 +0530

    hi

--- jack/src/main/java/exam/Run.java
+++ jack/src/main/java/exam/Run.java
@@ -1,19 +1,2 @@
 package exam;
 
-public class Run{
-    int speed;
-    void showSpeed() {
-        System.out.println("Current Speed : "+speed);
-    
-}
-public class SimpleTesting extends Run{
-    public static void main(String[] args) {
-        SimpleTesting run = new SimpleTesting();
-        run.showSpeed();
-        run.speed = 20;
-        run.showSpeed();
-        
-        run.speed;
-    }
-}
-}
